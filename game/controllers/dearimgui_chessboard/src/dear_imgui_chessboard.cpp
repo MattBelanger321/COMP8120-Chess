3e7737d3d54ac4a44023a914617a9057f7d530b4
Matthew Belanger <mb@tessonics.com>
2025-06-02 11:24:37 -0400

    better chess board

--- game/controllers/dearimgui_chessboard/src/dear_imgui_chessboard.cpp
+++ game/controllers/dearimgui_chessboard/src/dear_imgui_chessboard.cpp
@@ -1,5 +1,6 @@
 #include <dear_imgui_chessboard.hpp>
 
+#include <imgui.h>
 #include <iostream>
 
 namespace chess::controller {
@@ -17,7 +18,7 @@ namespace chess::controller {
     void imgui_chessboard::draw_board()
     {  // Use a unique ID for the child panel
         ImGui::BeginChild( "ChessboardPanel", ImVec2( static_cast< float >( width ), static_cast< float >( width ) ),
-                           true,  // border
+                           false,  // border
                            ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar |
                                ImGuiWindowFlags_NoScrollWithMouse );
         for ( int i = 0; i < 8; i++ ) {
@@ -36,7 +37,7 @@ namespace chess::controller {
         ImDrawList * draw_list = ImGui::GetWindowDrawList();
 
         // Choose a starting point (top-left corner of the square)
-        ImVec2 p           = ImGui::GetWindowPos();
+        ImVec2 p           = ImGui::GetCursorScreenPos();
         float  square_size = width / 8.f;
 
         // select the colour of the square
@@ -48,14 +49,12 @@ namespace chess::controller {
             colour = IM_COL32( 0, 0, 0, 255 );
         }
 
-        float thickness = width / 8.f;
-
         // Define the square corners
         ImVec2 top_left     = ImVec2( p.x + ( square_size * i ), p.y + ( square_size * j ) );
         ImVec2 bottom_right = ImVec2( top_left.x + square_size, top_left.y + square_size );
 
         // Draw the square
-        draw_list->AddRect( top_left, bottom_right, colour, 0.0f, ImDrawFlags_Closed, thickness );
+        draw_list->AddRectFilled( top_left, bottom_right, colour, 0.0f, ImDrawFlags_Closed );
     }
 
 }  // namespace chess::controller
