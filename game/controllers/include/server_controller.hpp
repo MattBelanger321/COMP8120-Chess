14070afba8d89d83b6d8e68f7b973890cca1b647
Matthew Belanger <matt_belanger1@outlook.com>
2025-07-16 14:20:07 -0400

    uncommented the networking

--- game/controllers/include/server_controller.hpp
+++ game/controllers/include/server_controller.hpp
@@ -3,26 +3,26 @@
 
 #include <atomic>
 #include <controller.hpp>
-// #include <server.hpp>
+#include <server.hpp>
 
 namespace chess::controller {
-    // class server_controller : public controller {
-    // public:
-    //     server_controller( int port );
-    //     ~server_controller();
+    class server_controller : public controller {
+    public:
+        server_controller( int port );
+        ~server_controller();
 
-    //     void start_listening();
-    //     void close();
+        void start_listening();
+        void close();
 
-    // private:
-    //     networking::server server;
-    //     std::atomic_flag   should_close;
+    private:
+        networking::server server;
+        std::atomic_flag   should_close;
 
-    //     std::thread listener_thread;
-    //     std::thread sender_thread;
+        std::thread listener_thread;
+        std::thread sender_thread;
 
-    //     void listen();
-    //     void send();
-    // };
+        void listen();
+        void send();
+    };
 }  // namespace chess::controller
 #endif
